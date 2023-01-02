6b3317af4a7dbf5f7834eda9ad4f431ac22829c6
Marcos Henrique <mhsilva555@gmail.com>
2023-01-02 12:21:53 -0300

    Add section Portfólio and navigation menu

--- js/app.js
+++ js/app.js
@@ -12,4 +12,59 @@ headline_button.addEventListener('click', () => {
         top: scrollDiv,
         behavior: 'smooth'
     });
-})
+});
+
+jQuery(function($) {
+    $('#nav-action').click(function(e) {
+        $(this).toggleClass('active');
+        let nav = $('.summary ul');
+
+        if ($(this).hasClass('active')) {
+            $(this).html('<i class="fa-solid fa-xmark"></i>');
+
+            nav.slideDown(200);
+        } else {
+            $(this).html('<i class="fa-solid fa-bars"></i>');
+
+            nav.slideUp(200);
+        }
+    });
+
+    $('.summary .nav-item').click(function(e) {
+        e.preventDefault();
+
+        let target = $(this).data('target');
+        let scrollDiv = document.getElementById(target).offsetTop;
+
+        window.scrollTo({
+            top: scrollDiv,
+            behavior: 'smooth'
+        });
+    });
+
+    $(document).ready(function(){
+        $(".carousel-portfolio").owlCarousel({
+            loop: true,
+            center: true,
+            lazyLoad: true,
+            items: 1,
+            margin: 0,
+            autoplay: true,
+            dots:true,
+            nav:true,
+            responsive: {
+            0: {
+                items: 1,
+                dots: false
+            },
+            768: {
+                items: 2,
+            },
+            1170: {
+                items: 2,
+                nav: false
+            }
+            }
+        });
+    });
+});
