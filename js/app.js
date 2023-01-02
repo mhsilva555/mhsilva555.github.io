3526fa3742c7c4dba88a8f1f197009b067fbb117
Marcos Henrique <mhsilva555@gmail.com>
2023-01-02 09:00:41 -0300

    Change style colors and Add scroll effect in javascript

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ progress.forEach((index) => {
     let = percentage = index.getAttribute('data-percentage')
     index.innerHTML = '<span style="width: '+percentage+'%">'+percentage+'%</span>'    
 });
+
+const headline_button = document.getElementById('headline-button');
+headline_button.addEventListener('click', () => {
+    let scrollDiv = document.getElementById("about").offsetTop;
+    window.scrollTo({
+        top: scrollDiv,
+        behavior: 'smooth'
+    });
+})
